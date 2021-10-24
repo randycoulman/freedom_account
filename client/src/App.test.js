b6571185c5763232923ab46610ee303bf4538f2a
Randy Coulman <rcoulman@gmail.com>
2021-10-24 01:21:31 -0700

    ⬆️ Upgrade client to latest create-react-app version
    Update client to match a newly-generated create-react-app app.
    
    Update eslint configuration to include latest versions of plugins and update rules to match.
    
    Left some things commented out for later, including Craco and eslint-plugin-ramda.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { render, screen } from "@testing-library/react";
 
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
 
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  expect(linkElement).toBeInTheDocument();
 });
