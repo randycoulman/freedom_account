4fd59b6c04c50f8e5f19ee0365c45cd485a51dd8
Randy Coulman <rcoulman@gmail.com>
2021-10-28 19:54:50 -0700

    ⬆️ Upgrade to Cypress 8.7.0
    Also regenerate boilerplate files

--- e2e/cypress/plugins/index.js
+++ e2e/cypress/plugins/index.js
@@ -1,3 +1,4 @@
+// / <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -11,7 +12,11 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (_on, _config) => {
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
