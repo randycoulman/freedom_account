ad9c581f2deedd0a46e021d3e5d178a953a6fc94
Randy Coulman <rcoulman@gmail.com>
2021-10-30 17:55:23 -0700

    ✨ Display a simple list of fake funds

--- client/src/FundList.tsx
+++ client/src/FundList.tsx
@@ -1,7 +1,38 @@
-const FundList = () => (
-  <article>
-    <h2>Funds</h2>
-  </article>
-);
+import { prop, sortBy } from "ramda";
+
+const fakeFunds = [
+  {
+    icon: "🏚️",
+    id: 1,
+    name: "Home Repairs",
+  },
+  {
+    icon: "🚘",
+    id: 2,
+    name: "Car Repairs",
+  },
+  {
+    icon: "💸",
+    id: 3,
+    name: "Property Taxes",
+  },
+];
+
+const FundList = () => {
+  const funds = sortBy(prop("name"), fakeFunds);
+
+  return (
+    <article>
+      <h2>Funds</h2>
+      <ul>
+        {funds.map((fund) => (
+          <li key={fund.id}>
+            {fund.icon} {fund.name}
+          </li>
+        ))}
+      </ul>
+    </article>
+  );
+};
 
 export default FundList;
--- e2e/cypress/integration/fund-list.spec.ts
+++ e2e/cypress/integration/fund-list.spec.ts
@@ -7,6 +7,11 @@ context("fund list", () => {
     it("shows a list of funds", () => {
       cy.findByRole("heading", { name: "Freedom Account" }).should("exist");
       cy.findByRole("heading", { name: "Funds" }).should("exist");
+      cy.findAllByRole("listitem").should((items) => {
+        expect(items[0]).to.contain.text("🚘 Car Repairs");
+        expect(items[1]).to.contain.text("🏚️ Home Repairs");
+        expect(items[2]).to.contain.text("💸 Property Taxes");
+      });
     });
   });
 });
