6851f264a72674836c6319a420a2d990774dd1cb
Randy Coulman <randy@mechanical-orchard.com>
2024-07-23 19:15:51 -0500

    🚨 Fix lint warnings

--- test/freedom_account/balances_test.exs
+++ test/freedom_account/balances_test.exs
@@ -11,13 +11,13 @@ defmodule FreedomAccount.BalancesTest do
     setup [:create_account]
 
     test "has zero balance when account has no funds", %{account: account} do
-      assert Money.zero?(Balances.account_balance(account))
+      assert account |> Balances.account_balance() |> Money.zero?()
     end
 
     test "has zero balance when all funds have no transactions", %{account: account} do
       for _i <- 1..3, do: Factory.fund(account)
 
-      assert Money.zero?(Balances.account_balance(account))
+      assert account |> Balances.account_balance() |> Money.zero?()
     end
 
     test "sums the balances of all funds in the account", %{account: account} do
