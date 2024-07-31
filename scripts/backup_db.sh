b0b9c9d821ee58c8bcd1af06e469e080654117f2
Randy Coulman <randy@randycoulman.com>
2024-07-30 19:28:25 -0700

    🧱 Add backup script and crontab
    Add postgres to `.tool-versions` so that `pg_dump` is available.
    
    Run `scripts/backup_db.sh install` to install the crontab entry; `scripts/backup_db.sh uninstall` to remove the crontab entry; `scripts/backup_db.sh` to create a date-stamped backup of the DB.
    
    The script deletes any backups older than 7 days, relying on Time Machine and remote backups to capture more history.
    
    Closes #175

---
+++ scripts/backup_db.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+case "${1-}" in
+  install)
+    crontab "${SCRIPT_DIR}/backup_db.cron"
+    echo "⬆️ crontab installed"
+    crontab -l
+    exit 0
+    ;;
+
+  uninstall)
+    crontab -r
+    echo "⬇️ crontab uninstalled"
+    crontab -l
+    exit 0
+    ;;
+esac
+
+BACKUP_DIR="${HOME}/backups"
+SECRETS="${SCRIPT_DIR}/../config/db_secret.env"
+
+set +a
+. ${SECRETS}
+set -a
+
+BACKUP_FILE="${BACKUP_DIR}/${POSTGRES_DB}_$(date +%Y%m%d).sql"
+
+mkdir -p $BACKUP_DIR
+
+PGPASSWORD="$POSTGRES_PASSWORD" pg_dump -h localhost -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_FILE"
+
+find "$BACKUP_DIR" -type f -name "*.sql" -mtime +7 -exec rm {} \;
