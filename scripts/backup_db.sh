e7db44ddc1200666a5071478e87d6f975f53eb72
Randy Coulman <randy@randycoulman.com>
2024-08-03 09:09:04 -0700

    🐛 Use full path to asdf
    Auto backups were still not working. This time, I set the crontab to run every 2 minutes and verified that it worked.

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -20,6 +20,7 @@ case "${1-}" in
     ;;
 esac
 
+ASDF="/opt/homebrew/opt/asdf/bin/asdf"
 BACKUP_DIR="${HOME}/backups"
 SECRETS="${SCRIPT_DIR}/../config/db_secret.env"
 
@@ -32,6 +33,6 @@ BACKUP_FILE="${BACKUP_DIR}/${POSTGRES_DB}_$(date +%Y%m%d).sql"
 mkdir -p $BACKUP_DIR
 
 cd ${SCRIPT_DIR}/..
-PGPASSWORD="$POSTGRES_PASSWORD" asdf exec pg_dump -h localhost -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_FILE"
+PGPASSWORD="$POSTGRES_PASSWORD" $ASDF exec pg_dump -h localhost -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_FILE"
 
 find "$BACKUP_DIR" -type f -name "*.sql" -mtime +7 -exec rm {} \;
