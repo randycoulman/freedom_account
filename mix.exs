144fddb81499311bafde804cd869ec79b6709554
Randy Coulman <randy@randycoulman.com>
2025-12-13 18:23:07 -0800

    🏷️ Release v1.11.0
    Remove some duplication within the compose.yml file while doing this.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule FreedomAccount.MixProject do
   # credo:disable-for-this-file Credo.Check.Warning.MixEnv
   # Reason: Use of Mix.env() is perfectly valid in this file.
 
-  @version "1.10.0"
+  @version "1.11.0"
 
   @spec project :: Keyword.t()
   def project do
