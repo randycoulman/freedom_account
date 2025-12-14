9fe6527859ef48efaf96484158414fdff17f284b
Randy Coulman <randy@randycoulman.com>
2025-12-13 17:17:20 -0800

    🚸 Auto-focus first form field in all forms
    Also, remove now-obsolete show/hide modal functions.

--- lib/freedom_account_web/components/core_components.ex
+++ lib/freedom_account_web/components/core_components.ex
@@ -444,7 +444,7 @@ defmodule FreedomAccountWeb.CoreComponents do
         {@title}
         <:subtitle>{render_slot(@subtitle)}</:subtitle>
       </.header>
-      <.form :let={f} class="w-full" for={@for} as={@as} {@rest}>
+      <.form :let={f} class="w-full" for={@for} as={@as} phx-mounted={JS.focus_first()} {@rest}>
         {render_slot(@inner_block, f)}
         <footer class="flex gap-4 justify-center mt-6">
           <div :for={action <- @actions}>
@@ -547,18 +547,6 @@ defmodule FreedomAccountWeb.CoreComponents do
     )
   end
 
-  def hide_modal(js \\ %JS{}, id) do
-    js
-    |> JS.hide(
-      to: "##{id}-bg",
-      transition: {"transition-all transform ease-in duration-200", "opacity-100", "opacity-0"}
-    )
-    |> hide("##{id}-container")
-    |> JS.hide(to: "##{id}", transition: {"block", "block", "hidden"})
-    |> JS.remove_class("overflow-hidden", to: "body")
-    |> JS.pop_focus()
-  end
-
   def show(js \\ %JS{}, selector) do
     JS.show(js,
       to: selector,
@@ -567,19 +555,6 @@ defmodule FreedomAccountWeb.CoreComponents do
         {"transition-all ease-out duration-300", "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95",
          "opacity-100 translate-y-0 sm:scale-100"}
     )
-  end
-
-  def show_modal(js \\ %JS{}, id) when is_binary(id) do
-    js
-    |> JS.show(to: "##{id}")
-    |> JS.show(
-      to: "##{id}-bg",
-      time: 300,
-      transition: {"transition-all transform ease-out duration-300", "opacity-0", "opacity-100"}
-    )
-    |> show("##{id}-container")
-    |> JS.add_class("overflow-hidden", to: "body")
-    |> JS.focus_first(to: "##{id}-content")
   end
 
   @doc """
